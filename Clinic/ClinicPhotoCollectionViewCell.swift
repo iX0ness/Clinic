7e21dcd63b0ebf6b5ad9f867ada1be783c92c8ea
Levchuk Misha <mykhaylo.levchuk@ukr.net>
2017-10-04 15:21:23 +0200

    photo slider

---
+++ Clinic/ClinicPhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ClinicPhotoCollectionViewCell.swift
+//  Clinic
+//
+//  Created by Levchuk Misha on 04.10.17.
+//  Copyright © 2017 Levchuk Misha. All rights reserved.
+//
+
+import UIKit
+
+class ClinicPhotoCollectionViewCell: UICollectionViewCell {
+    
+}
